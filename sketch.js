e22b9949832376a0f50917b89199c18f830cbb10
110318 <andrescamilogomezamaya@gmail.com>
2022-10-12 21:43:17 -0500

    Implementación listado de 16 personalidades

--- sketch.js
+++ sketch.js
@@ -1,7 +1,86 @@
+let bolas = [];
+let puntaje = 0;
+let letras = ["I","E","N","S","T","F","J","P"]
+let tipados = [{
+  puntaje: 0,
+  personalidad: "INTJ",
+},
+{
+  puntaje: 0,
+  personalidad: "INTP"
+},
+{
+  puntaje:0,
+  personalidad: "ENTJ"
+},
+{
+  puntaje:0,
+  personalidad: "ENTP"
+},
+{
+  puntaje:0,
+  personalidad: "INFJ"
+},
+{
+  puntaje:0,
+  personalidad: "INFP"
+},
+{
+  puntaje:0,
+  personalidad: "ENFJ"
+},
+{
+  puntaje:0,
+  personalidad: "ENFP"
+},
+{
+  puntaje:0,
+  personalidad: "ISTJ"
+},
+{
+  puntaje:0,
+  personalidad: "ISFJ"
+},
+{
+  puntaje:0,
+  personalidad: "ESTJ"
+},
+{
+  puntaje:0,
+  personalidad: "ESFJ"
+},
+{
+  puntaje:0,
+  personalidad: "ISTP"
+},
+{
+  puntaje:0,
+  personalidad: "ISFP"
+},
+{
+  puntaje:0,
+  personalidad: "ESTP"
+},
+{
+  puntaje:0,
+  personalidad: "ESFP"
+}
+];
+
+
 function setup() {
   createCanvas(1920, 1080);
+ 
 }
 
 function draw() {
   background(220);
+  fill(0);
+  text("presiona en los botones para" + " "+ "empezar/pausar el video",20,100);
+  ellipse(50,50,100);
+
+
+
+
+
 }
