d947b2b28e11abf064f2365d7ed3163a673e7e8b
110318 <andrescamilogomezamaya@gmail.com>
2022-10-12 22:50:36 -0500

    Implementaición función video

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 let bolas = [];
 let puntaje = 0;
 let letras = ["I","E","N","S","T","F","J","P"]
+let vidElement;
+let playBtn;
+let stopBtn;
 let tipados = [{
   puntaje: 0,
   personalidad: "INTJ",
@@ -70,17 +73,36 @@ let tipados = [{
 
 function setup() {
   createCanvas(1920, 1080);
+  vidElement = createVideo("videos/video1.mp4");
+  vidElement.position(200,50);
+  vidElement.size(1000);
+
+  playBtn = createButton("playvideo");
+  playBtn.position(30,40);
+  playBtn.mouseClicked(playVideo);
+
+  stopBtn = createButton("pausevideo");
+  stopBtn.position(150,40);
+  stopBtn.mouseClicked(pauseVideo);
+  
+  
  
 }
 
 function draw() {
   background(220);
+
   fill(0);
   text("presiona en los botones para" + " "+ "empezar/pausar el video",20,100);
-  ellipse(50,50,100);
+  //ellipse(50,50,100);
+
+}
+
+function playVideo(){
+  vidElement.play();
+}
 
 
-
-
-
+function pauseVideo(){
+  vidElement.pause();
 }
